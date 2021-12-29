a84d6f49ef9555b80665e31717a983ecf3f97d94
kwb020312 <kwb020312@naver.com>
2021-12-29 23:35:43 +0900

    #2.3 Server Side Rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
-export default function App({ Component, Props }) {
+export default function App({ Component, pageProps }) {
 	return (
 		<Layout>
-			<Component {...Props} />
+			<Component {...pageProps} />
 		</Layout>
 	);
 }
