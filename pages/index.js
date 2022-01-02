0b6b513b2686eac3d4c85beaaa94415c0c6949e2
kwb020312 <kwb020312@naver.com>
2022-01-02 23:37:52 +0900

    #2.4 Recap

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({results}) {
             {!results && <h4>Loading...</h4>}
             {results?.map(movie => (
                 <div className="movie" key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="Movie Poster" />
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="Movie Poster" />
                     <h4>{movie.original_title}</h4>
                 </div>
             ))}
@@ -17,6 +17,9 @@ export default function Home({results}) {
                     grid-template-columns: 1fr 1fr;
                     padding: 20px;
                     gap: 20px;
+                }
+                .movie {
+                    cursor: pointer;
                 }
                 .movie img {
                     max-width: 100%;
